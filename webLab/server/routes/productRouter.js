d5ddb75444640c4de6d4bf820987a00b09547c3e
EvgenyKuznetsov153502 <15350071@study.bsuir.by>
2023-12-13 23:31:52 +0300

    end of the main part of the server

--- webLab/server/routes/productRouter.js
+++ webLab/server/routes/productRouter.js
@@ -1,12 +1,13 @@
 const Router = require('express')
 const router = new Router()
 const productController = require('../controllers/productController')
+const checkRole = require('../middleware/checkRoleMiddleware')
 
-router.post('/', productController.create)
+router.post('/',checkRole("ADMIN"), productController.create)
 router.get('/', productController.getAll)
 router.get('/:id',productController.getOne)
-router.delete('/:id', productController.delete)
-router.put('/:id', productController.update)
+router.delete('/:id',checkRole("ADMIN"), productController.delete)
+router.put('/:id',checkRole("ADMIN"), productController.update)
 
 module.exports = router
 
